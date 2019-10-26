b3f606ade649c410a73f7ccea6824b29edc54108
Alexandra <nerenisa@gmail.com>
2019-10-26 15:40:17 +0300

    new

--- Hallowen_mission.py
+++ Hallowen_mission.py
@@ -2,67 +2,54 @@
 # coding: utf-8
 
 import re
-from itertools import combinations
-import itertools
+# from collections import Counter
+# from copy import deepcopy
 
 MONSTERS = ['skeleton', 'ghost', 'jack', 'vampire', 'witch', 'mummy', 'zombie', 'werewolf', 'frankenstein'] 
 
 
 
-
-
-
-
-
-# MONSTERS = '''
-# skeleton
-# ghost
-# jack
-# vampire
-# witch
-# mummy
-# zombie
-# werewolf
-# frankenstein
-# '''
-
-
 def halloween_monsters(spell: str)-> int:
-    s1 = []
-    for letter_2 in MONSTERS:
+   s1 = []
+   s2 = []
+   for letter_2 in MONSTERS:
         Res = ''.join([x for x in letter_2 if x in spell])
-        s2 = []
-        if Res > letter_2:
-           s2.append(Res)
-            
+        if Res != letter_2:
+           s2.append(Res) 
         elif Res == letter_2:
            s1.append(Res) 
                  
-    
-    d = ' '.join((s1))
-    if 'ghost' in d:
-        repl = d.replace('jack', 'ghost') 
-        return len(repl.split())
-    elif 'witch' in d:
-        dubl = ' '.join(('witch', d)) 
-        return len(dubl.split())
-    elif 'skeleton' in d:
-        dell = ' '.join(((re.sub(r'\s+', ' ', (d.replace('jack', ''))), '(not jack)')))
-        return len(dell.split(maxsplit=1))
-    elif 'vampire' in d:
-        dell_1 = ' '.join((''.join(((re.sub(r'\s+', ' ', (d.replace('mummy', ''))), 'vampire'))), '(not mummy)'))
-        return len(dell_1.split(maxsplit=1))
-    else:
+   k = ' '.join(''.join(s2))  
+   d = ' '.join((s1))
+   try:
+      if s2 not in MONSTERS:
+         return 0
+   finally:
+      if 'ghost' in d:
+        return len (d.replace('jack', 'ghost').split())
+      elif 'witch' in d:
+        return len(' '.join(('witch', d)).split())
+      elif 'skeleton' in d:
+        return len(' '.join(((re.sub(r'\s+', ' ', (d.replace('jack', ''))), '(not jack)'))).split(maxsplit=1))
+      elif 'vampire' in d:
+        return len(' '.join((''.join(((re.sub(r'\s+', ' ', (d.replace('mummy', ''))), 'vampire'))), '(not mummy)')).split(maxsplit=1))
+      else:
         return len(d.split())
-
-   
 
 
 
 if __name__ == '__main__':
-    assert halloween_monsters('casjokthg') == 2, 'jack ghost'
-    assert halloween_monsters('leumooeeyzwwmmirbmf') == 3, 'mummy zombie werewolf'
-    assert halloween_monsters('nafrweiicttwneshhtikcn') == 3, 'witch witch frankenstein'
-    assert halloween_monsters('kenoistcepajmlvre') == 2, 'skeleton vampire (not jack)'
-    assert halloween_monsters('miaimavrurymepepv') == 2, 'vampire vampire (not mummy)'
-    print("Your spell seem to be okay. It's time to check.")
+   assert halloween_monsters('casjokthg') == 2, 'jack ghost'
+   assert halloween_monsters('leumooeeyzwwmmirbmf') == 3, 'mummy zombie werewolf'
+   assert halloween_monsters('nafrweiicttwneshhtikcn') == 3, 'witch witch frankenstein'
+   assert halloween_monsters('kenoistcepajmlvre') == 2, 'skeleton vampire (not jack)'
+   assert halloween_monsters('miaimavrurymepepv') == 2, 'vampire vampire (not mummy)'
+   print("Your spell seem to be okay. It's time to check.")
+
+
+
+
+
+
+
+
